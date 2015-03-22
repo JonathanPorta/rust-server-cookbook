0d25d0eb2cba8f3e9a3aeea36a50f2082026125e
Jonathan Porta <jonathan@jonathanPorta.com>
2015-03-22 01:13:37 -0700

    updates

--- resources/steamcmd.rb
+++ resources/steamcmd.rb
@@ -1,7 +1,7 @@
 actions :install, :update
 default_action :install
 
-attribute :app_id, name_attribute: true, kind_of: Fixnum, required: true
+attribute :app_id, name_attribute: true, kind_of: String, required: true
 
 attribute :path, kind_of: String, required: true
 attribute :beta, kind_of: String
