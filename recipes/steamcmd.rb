9da275e9058545102f4c1da904ec8a0d9ce58f76
Jonathan Porta <jonathan@jonathanPorta.com>
2015-03-22 03:58:16 -0700

    updates

--- recipes/steamcmd.rb
+++ recipes/steamcmd.rb
@@ -26,14 +26,14 @@ end
 template 'c:/rust-server/start.ps1' do
   source 'rust-server.ps1'
   variables {
-    install_path: 'c:/rust-server/'
-    name: '~aaaamazing PVE Server - No Sleepers - Noob friendly - rust.rurd4me.com'
-    maxplayers: 50
-    port: 28055
-    identity: 'server'
-    seed: 696969
-    worldsize: 4000
-    rcon_port: 5718
-    rcon_password: ''
+    install_path: 'c:/rust-server/',
+    name: '~aaaamazing PVE Server - No Sleepers - Noob friendly - rust.rurd4me.com',
+    maxplayers: 50,
+    port: 28055,
+    identity: 'server',
+    seed: 696969,
+    worldsize: 4000,
+    rcon_port: 5718,
+    rcon_password: '',
     rcon_ip: ''
   }
