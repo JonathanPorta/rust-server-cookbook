0adb6360e42c01e47968e4b5899cd03faaaf8494
Jonathan Porta <jonathan@jonathanPorta.com>
2015-03-23 01:26:34 -0700

    changes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-::Chef::Recipe.send(:include, Chef::OpenSSL::Password)
+# ::Chef::Recipe.send(:include, Chef::OpenSSL::Password)
 include_recipe 'rust::steamcmd'
 include_recipe 'nssm'
 
@@ -35,7 +35,7 @@ template 'c:/rust-server/start.ps1' do
     seed: 696969,
     worldsize: 4000,
     rcon_port: 5718,
-    rcon_password: secure_password(40),
+    rcon_password: '',
     rcon_ip: '127.0.0.1'
   })
 end
